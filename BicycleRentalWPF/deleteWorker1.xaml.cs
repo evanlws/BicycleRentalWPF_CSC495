6b2b7762f07094d7ffe9751ab6b114d133398862
Van <van.le@pridetek.com>
2016-05-01 23:09:16 -0400

    Finish Gui

---
+++ BicycleRentalWPF/deleteWorker1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BicycleRentalWPF
+{
+    /// <summary>
+    /// Interaction logic for deleteWorker.xaml
+    /// </summary>
+    public partial class deleteWorker : Window
+    {
+        public deleteWorker()
+        {
+            InitializeComponent();
+        }
+    }
+}
